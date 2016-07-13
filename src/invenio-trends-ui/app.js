2f1b60a240f2c9cc864c724de43cfaf616e8abbc
Teo Stocco <teo.stocco@epfl.ch>
2016-07-13 14:11:01 +0200

    add basic data graph

--- src/invenio-trends-ui/app.js
+++ src/invenio-trends-ui/app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import angular from "angular";
-
 /*
   - app.controllers.js
   - app.directives.js
@@ -10,10 +8,20 @@ import angular from "angular";
   - app.services.js
 */
 
-const templateUrl = 'trends-search.html';
-
+import angular from "angular";
 import nv from 'nvd3';
 import d3 from 'd3';
+
+const templateUrl = 'trends-search.html';
+const apiUrl = 'http://localhost:9000/search';
+
+const controller = ($scope, $log) => {
+
+  $scope.search = () => {
+    $log.log($scope.searchInput)
+  };
+
+};
 
 nv.addGraph(() => {
   var chart = nv.models.lineWithFocusChart();
@@ -22,9 +30,25 @@ nv.addGraph(() => {
   chart.yAxis.tickFormat(d3.format(',.2f'));
   chart.y2Axis.tickFormat(d3.format(',.2f'));
 
+  const data = [
+    {
+      key: "label",
+      values: [
+        {
+          x: 1,
+          y: 3.5
+        },
+        {
+          x: 5,
+          y: 3.0
+        }
+      ]
+    }
+  ];
+
   d3
     .select('#graph svg')
-    .datum(testData())
+    .datum(data)
     .transition()
     .duration(500)
     .call(chart);
@@ -34,68 +58,10 @@ nv.addGraph(() => {
   return chart;
 });
 
-
-function testData() {
-  return stream_layers(3,128,.1).map(function(data, i) {
-    return {
-      key: 'Stream' + i,
-      values: data
-    };
-  });
-}
-
-function stream_layers(n, m, o) {
-  if (arguments.length < 3) o = 0;
-  function bump(a) {
-    var x = 1 / (.1 + Math.random()),
-      y = 2 * Math.random() - .5,
-      z = 10 / (.1 + Math.random());
-    for (var i = 0; i < m; i++) {
-      var w = (i / m - y) * z;
-      a[i] += x * Math.exp(-w * w);
-    }
-  }
-  return d3.range(n).map(function() {
-    var a = [], i;
-    for (i = 0; i < m; i++) a[i] = o + o * Math.random();
-    for (i = 0; i < 5; i++) bump(a);
-    return a.map(stream_index);
-  });
-}
-
-/* Another layer generator using gamma distributions. */
-function stream_waves(n, m) {
-  return d3.range(n).map(function(i) {
-    return d3.range(m).map(function(j) {
-      var x = 20 * j / m - i / 3;
-      return 2 * x * Math.exp(-.5 * x);
-    }).map(stream_index);
-  });
-}
-
-function stream_index(d, i) {
-  return {x: i, y: Math.max(0, d)};
-}
-
-
-const controller = ($scope, $log) => {
-
-  $scope.hello = 'world';
-  $scope.update = () => {
-    $log.log($scope.input)
-  };
-
-};
-
 angular
   .module('invenio-trends', [])
   .component('trendsSearch', {
     templateUrl,
     controller
   });
-
-
-
-
-
 
